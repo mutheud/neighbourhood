9f0179f5e31e1ba5473f4667cac2575f9345dc26
Ghost Rider <dianamulungi@gmail.com>
2019-01-16 15:24:03 +0300

    added forms for the classes

---
+++ watch/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Business,Profile,NeighborHood,Post,Business,Comment
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user','profile','neighborHood']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user','neighborhood','business']
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighborHood
+        exclude= ['occupants']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user','neighborHood','pub_date']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name']
